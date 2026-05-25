3ba42bbd7466cfb65848870042e0ec232ce21fa3
Lyftads <info@lyftads.agency>
2026-05-26 00:45:14 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -606,7 +606,9 @@ function WeeklyTab({ weeks, data, metaWeekly, shopifyWeekly, onUpdate, cfg, S })
   const totSes = sum('ses')
 
   const avgMER = div(totFat, totAdv)
-const avgAMer = div(totFatNC, totAdv)
+const avgAMER = div(totFatNC, totAdv)
+const avgAMer = avgAMER
+
 const avgCAC = div(totAdv, totNC)
 const avgCPO = div(totAdv, totOrd)
 
